f191b9e9f57caf53b0e903345de9f61750e6657c
lby1024 <1028524431@qq.com>
2018-06-13 18:26:33 +0800

    最简单的洋葱圈

---
+++ 最简单的一个洋葱圈.js
@@ -0,0 +1,24 @@
+function fn1 (next) {
+    console.log("---fn1进入---")
+    next()
+    console.log("---fn1出去---")
+}
+function fn2 (next) {
+    console.log("---fn2进入---")
+    next()
+    console.log("---fn2出去---")
+}
+function fn3 (next) {
+    console.log("---fn3---")
+}
+
+let list = [fn1, fn2, fn3]
+let number = 0
+
+function next () {
+    number++
+    list[number](next)
+}
+
+// 开始执行
+fn1(next)
